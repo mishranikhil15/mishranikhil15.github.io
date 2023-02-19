c9580bdcc3b254ac061fabcc3586a703afaea261
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-02-19 16:24:29 +0530

    final3

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
- document.getElementById('resume-button-1').addEventListener("click",()=>{
+ document.getElementById('resume-link-1').addEventListener("click",()=>{
     // console.log("OPENinig.....")
     window.location.assign("https://drive.google.com/file/d/1fD7AWQO8sJAaH3oqe1sK7ix1Ou5moPiO/view?usp=share_link", "_blank");
   })
   
-  document.getElementById('resume-button-2').addEventListener("click",()=>{
+  document.getElementById('resume-link-2').addEventListener("click",()=>{
     // console.log("OPENinig.....")
     window.location.assign("https://drive.google.com/file/d/1fD7AWQO8sJAaH3oqe1sK7ix1Ou5moPiO/view?usp=share_link", "_blank");
   }) 
