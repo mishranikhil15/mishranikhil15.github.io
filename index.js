603fcfbdac50c415e2ac94a07256c70c09c4d511
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-03-20 23:15:43 +0530

    done

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+let sidemenu=document.getElementById("sidemenu");
 
+function openmenu(){
+    sidemenu.style.right="0";
+}
+function closemenu(){
+    sidemenu.style.right="-200px";
+}
+ 
+ 
+ 
  document.getElementById('resume-link-1').addEventListener("click",()=>{
     // console.log("OPENinig.....")
     window.location.assign("https://drive.google.com/file/d/1fD7AWQO8sJAaH3oqe1sK7ix1Ou5moPiO/view?usp=share_link", "_blank");
