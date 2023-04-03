7a6a9875a18434c5ab88a4f1b2d2e5e82b85f77e
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-04-04 00:14:56 +0530

    resumeupdate

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ var typed = new Typed(".typing", {
  
  document.getElementById('resume-link-1').addEventListener("click",()=>{
     // console.log("OPENinig.....")
-    window.open("https://drive.google.com/file/d/1fD7AWQO8sJAaH3oqe1sK7ix1Ou5moPiO/view?usp=share_link", "_blank");
+    window.open("https://drive.google.com/file/d/1nh40oouprCxrFfmu3hcV4ONvMf1wLZ63/view?usp=sharing", "_blank");
   })
   
   document.getElementById('resume-link-2').addEventListener("click",()=>{
     // console.log("OPENinig.....")
-    window.open("https://drive.google.com/file/d/1fD7AWQO8sJAaH3oqe1sK7ix1Ou5moPiO/view?usp=share_link", "_blank");
+    window.open("https://drive.google.com/file/d/1nh40oouprCxrFfmu3hcV4ONvMf1wLZ63/view?usp=sharing", "_blank");
   }) 
